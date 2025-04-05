b52e68d0e45dd1fcd66c4b92d03369996cd7759b
Bhuvaneshkumar1 <92296241+Bhuvaneshkumar1@users.noreply.github.com>
2025-04-05 19:14:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,11 +26,32 @@ document.getElementById('searchButton').addEventListener('click', function() {
             document.getElementById('weatherResult').innerHTML = ''; // Clear previous results
 
             const weatherResult = `
-                <h2>${data.name}</h2>
-                <p>Temperature: ${data.main.temp} °C</p>
-                <p>Humidity: ${data.main.humidity} %</p>
-                <p>Condition: ${data.weather[0].description}</p>
-                <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="Weather Icon">
+                <div class="weather-card">
+                    <h2>${data.name} <span class="country">${data.sys.country}</span></h2>
+                    <div class="current-weather">
+                        <img class="weather-icon" src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Weather Icon">
+                        <div class="temp-main">${Math.round(data.main.temp)}°C</div>
+                        <div class="condition">${data.weather[0].description}</div>
+                    </div>
+                    <div class="weather-details">
+                        <div class="weather-detail">
+                            <i class="fas fa-temperature-low"></i>
+                            <div>Feels Like: ${Math.round(data.main.feels_like)}°C</div>
+                        </div>
+                        <div class="weather-detail">
+                            <i class="fas fa-tint"></i>
+                            <div>Humidity: ${data.main.humidity}%</div>
+                        </div>
+                        <div class="weather-detail">
+                            <i class="fas fa-wind"></i>
+                            <div>Wind: ${data.wind.speed} m/s</div>
+                        </div>
+                        <div class="weather-detail">
+                            <i class="fas fa-compress-alt"></i>
+                            <div>Pressure: ${data.main.pressure} hPa</div>
+                        </div>
+                    </div>
+                </div>
             `;
             document.getElementById('weatherResult').innerHTML = weatherResult; // Display weather data
 
